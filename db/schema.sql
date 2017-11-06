152acd1995320f8c6e43363e1c3bc87fa7ac4b19
mathiskim <quenbess@gmail.com>
2017-11-05 18:44:32 -0600

    first run at sql db and tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,38 @@
-CREATE DATABASE beeProductive;
-USE DATABASE beeProductive;
-CREATE TABLE users (
-id INT AUTO_INCREMENT NOT NULL,
-userName VARCHAR(255),
-hasBug1 BOOLEAN,
-numberBug1 INT (5),
-hasBug2 BOOLEAN,
-numberBug2 INT (5),
-hasBug3 BOOLEAN,
-numberBug3 INT (5),
-hasBug4 BOOLEAN,
-numberBug4 INT (5),
-createdAt TIMESTAMP NOT NULL
-)
+### SQL Schema
+DROP DATABASE IF EXISTS `BeeProductive`;
+
+CREATE DATABASE BeeProductive;
+USE BeeProductive;
+
+CREATE TABLE Users
+(
+	id int NOT NULL AUTO_INCREMENT,
+	UserName varchar(255) NOT NULL,
+   HasBug1 BOOLEAN DEFAULT false,
+   HasBug2 BOOLEAN DEFAULT false,
+   HasBug3 BOOLEAN DEFAULT false,
+   HasBug4 BOOLEAN DEFAULT false,
+	DateCreated  datetime DEFAULT NOW(),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Bugs
+(
+	id int NOT NULL AUTO_INCREMENT,
+	BugName varchar(255) NOT NULL,
+   BugWorth int,
+   BugRarity int,
+	DateCreated  datetime DEFAULT NOW(),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE UserBugs
+(
+	id int NOT NULL AUTO_INCREMENT,
+   UserID int NOT NULL,
+	BugLabel varchar(255) NOT NULL,
+   BugDescription varchar(255),
+   TimeSpentInMinutes int,
+	DateCreated  datetime DEFAULT NOW(),
+	PRIMARY KEY (id)
+);
