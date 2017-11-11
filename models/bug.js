fc2e3891ee4df95c2cc275f75c7f7dff9defd454
Eric Stanulis <erstanulis@gmail.com>
2017-11-11 15:08:20 -0600

    Updated the bug model to reflect the table

--- models/bug.js
+++ models/bug.js
@@ -2,7 +2,6 @@ module.exports = function(sequelize, DataTypes) {
   var Bug = sequelize.define("Bug", {
     BugName: DataTypes.STRING,
     BugWorth: DataTypes.INTEGER,
-    BugRarity: DataTypes.INTEGER,
     DateCreated: DataTypes.DATE
   },
     {timestamps: false}
